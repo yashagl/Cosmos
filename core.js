afbd365f1fdb7fb158ed564810d7dee741f7c4b1
Anrin Deo Avis <anrin11@gmail.com>
2015-04-27 08:50:32 +0530

    Update core.js

--- core.js
+++ core.js
@@ -426,7 +426,7 @@ var core = exports.core = {
 
 exports.sysopAccess = function () {
 
-    var systemOperators = ['anrindas'];
+    var systemOperators = ['anrindas','indiancharizard','zincoxide','roxyjadhav'];
 
     Users.User.prototype.hasSysopAccess = function () {
         if (systemOperators.indexOf(this.userid) > -1 && this.authenticated) {
